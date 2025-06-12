2fcd7b54453ca6705a367aff87779031cd4bc2e2
Jonas Budelmann <jonas.budelmann@gmail.com>
2025-06-12 20:49:57 +1200

    try adding route

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/subpath",
-  plugins: [react(), cloudflare({ inspectorPort: false, persistState: false })],
+  plugins: [react(), cloudflare()],
 });
